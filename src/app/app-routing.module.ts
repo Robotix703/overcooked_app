d0d312c140ae47b9ecde6db0656868f35c1af18e
Robotix703 <stephane.chatelle@orange.fr>
2023-01-21 21:10:03 +0100

    Add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,38 @@ import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
-    canLoad: [AuthGuard]
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canLoad: [AuthGuard]
+  },
+  {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'meal',
+    loadChildren: () => import('./meal/meal.module').then( m => m.MealPageModule)
+  },
+  {
+    path: 'pantry/list',
+    loadChildren: () => import('./pantry/list/list.module').then( m => m.ListPageModule)
+  },
+  {
+    path: 'pantry/create',
+    loadChildren: () => import('./pantry/create/create.module').then( m => m.CreatePageModule)
+  },
+  {
+    path: 'todoist/create',
+    loadChildren: () => import('./todoist/create/create.module').then( m => m.CreatePageModule)
+  },
+  {
+    path: 'todoist/list',
+    loadChildren: () => import('./todoist/list/list.module').then( m => m.ListPageModule)
   }
 ];
 
