dbee2eee88b6f88fd1742124b2e19478153f7079
Robotix703 <stephane.chatelle@orange.fr>
2023-01-27 16:24:05 +0100

    Add structure of home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Recipe } from '../recipe/recipe.model';
+import { RecipeService } from '../recipe/recipe.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  recipeSub: Subscription;
+  recipes: Recipe[];
 
+  constructor(private recipeService: RecipeService) {}
+
+  display(data: Recipe[]){
+    this.recipes = data;
+  }
+
+  ngOnInit() {
+    this.recipeSub = this.recipeService.getRecipes().subscribe(data => {
+      this.display(data.recipes);
+    });
+  }
+
+  onIonInfinite(ev) {
+    //TODO - Update limitedList
+
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
+
+  createMeal(recipeId: string){
+
+  }
 }
---
+++ src/app/recipe/recipe.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Recipe } from './recipe.model';
+
+const URL_BACKEND = environment.apiURL + 'recipe';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+  constructor(private http: HttpClient) { }
+
+  getRecipes(): Observable<{recipes: Recipe[], count: number}>{
+    return this.http.get<{recipes: Recipe[], count: number}>(URL_BACKEND, {});
+  }
+}
