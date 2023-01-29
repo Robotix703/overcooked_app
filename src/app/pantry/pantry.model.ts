5aff4be2e48ff982d4d484f938ae669da91de102
Robotix703 <stephane.chatelle@orange.fr>
2023-01-29 16:45:23 +0100

    Fix follow page

--- src/app/pantry/pantry.model.ts
+++ src/app/pantry/pantry.model.ts
@@ -11,5 +11,6 @@ export interface IngredientInventory {
   ingredientName: string,
   ingredientImagePath: string,
   ingredientFreezable: boolean,
+  ingredientUnitOfMeasure: string,
   pantries: Pantry[]
 }
