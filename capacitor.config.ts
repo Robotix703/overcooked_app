0bd1bf0bda6a5f8e9f832068163573c71fc9177a
Robotix703 <stephane.chatelle@orange.fr>
2023-02-03 10:45:31 +0100

    Change app name Fix#2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'fr.robotix703.overcooked',
-  appName: 'overcooked_app',
+  appName: 'overcooked',
   webDir: 'www',
   bundledWebRuntime: false
 };
