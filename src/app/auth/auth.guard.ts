99382b6126b93abc5dcb4a4d85c1b6a7ee17e309
Robotix703 <stephane.chatelle@orange.fr>
2024-01-27 13:25:58 +0000

    Update Angular dependencies to version 16.2.12

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,6 @@
 /* eslint-disable arrow-body-style */
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
+import { Route, UrlSegment, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthService } from './auth.service';
@@ -8,7 +8,7 @@ import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canLoad(
